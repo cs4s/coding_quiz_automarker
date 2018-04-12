98c68c8d81cf1157ff8576b9db5c68e484ada4c1
Daniel Hickmott <daniel.hickmott@gmail.com>
2018-04-12 18:27:22 +1000

    Changes marker.py to modify some response headings

--- marker.py
+++ marker.py
@@ -1,5 +1,14 @@
 import csv
+import re
 
+def modify_responses_heading(heading):
+
+	response_heading = ''
+	question_match = re.match(r'https:\/\/cs4s.github.io\/cc_quiz\/(\w+)\/q(\d+)\/question(\w*).png', heading)
+	if question_match:
+		return '{}_{}'.format(question_match.group(1), question_match.group(2))
+
+	return response_heading
 
 def get_quiz_responses():
 
@@ -16,11 +25,23 @@ def get_quiz_responses():
 
 	return quiz_rows
 
-
+# Counter to keep track of how many quiz question headers have been modified
+question_count = 0
 
 modified_responses = []
 quiz_responses = get_quiz_responses()
-print(quiz_responses)
 
+for (index, row) in enumerate(quiz_responses):
+	modified_row = []
+	for cell in row:
+		modified_cell = ''
+		# The logic for modifying the responses header row is different to the other rows
+		if index == 0:
+			modified_cell = modify_responses_heading(cell)
 
+		modified_row.append(modified_cell)
 
+	modified_responses.append(modified_row)
+
+# Write out the modified responses to a csv
+print(modified_responses)
